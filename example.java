402917b856e4d63b432fc6343dc4cf74cc15a0dc
Giriprasanth <158739934+Giriprasanth-Kumar@users.noreply.github.com>
2025-02-13 15:58:47 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,49 @@
+class example
+{
+    public static void main(String[] args) {
+        int a=123456,res,ee=0,oo=0,pp=0;
+        int even = 0,odd = 0,prime =0;
+        while(a>0)
+        {
+            res=a%10;
+            a=a/10;
+            if(res%2==0)
+            {
+                even+=res;
+                ee=ee*10+res;
+            }
+            else
+            {
+                odd+=res;
+                oo=oo*10+res;
+            }
+            if(res==2)
+            {
+                prime+=res;
+                pp=pp*10+res;
+            }
+            else if(res>2)
+            {
+               int count=0; 
+               for(int i=2;i<res;i++)
+               {
+                if(res%i==0 && count<1)
+                {
+                    count++;
+                }   
+               }
+               if(count<1)
+               {
+                  prime+=res;
+                  pp=pp*10+res;
+               }
+            }
+        }
+        System.out.println("The sum of even numbers:"+even);
+        System.out.println("The sum of odd numbers:"+odd);
+        System.out.println("The sum of prime numbers:"+prime);
+        System.out.println("Even numbers in reverse order:"+ee);
+        System.out.println("Odd numbers in reverse order:"+oo);
+        System.out.println("Prime numbers in reverse order:"+pp);
+    }
+}
